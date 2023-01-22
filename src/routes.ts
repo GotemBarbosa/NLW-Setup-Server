4005c46455a9f8eaf688e38dca7d10d52437561d
GotemBarbosa <gotembarbosagd@gmail.com>
2023-01-22 00:29:10 -0300

    5 errors fix

--- src/routes.ts
+++ src/routes.ts
@@ -66,72 +66,62 @@ export async function appRoutes(app: FastifyInstance){
 
         const completedHabits = day?.dayHabits.map(dayHabit => {
             return dayHabit.habit_id
-        })
+        }) ?? []
         return{
             possibleHabits,
             completedHabits
         }
     })
 
-    app.patch('/habits/:id/toogle', async(request)=>{
-        // route param => parâmetro de identificação
-
-        const toogleHabitParams = z.object({
-            id: z.string().uuid(),
-        }) 
-
-        const { id } = toogleHabitParams.parse(request.params)
-
+    app.patch("/habits/:id/toggle", async (request) => {
+        const toggleHabitParams = z.object({
+          id: z.string().uuid(),
+        });
+    
+        const { id } = toggleHabitParams.parse(request.params);
+        
         //validação para ser no mesmo dia
-        const today = dayjs().startOf('day').toDate()
-
+        const today = dayjs().startOf("day").toDate();
+    
         let day = await prisma.day.findUnique({
-            where:{
-                date: today
-            }
-        })
-
-        if(!day){
-            day = await prisma.day.create({
-                data:{
-                    date: today,
-                }
-            })
+          where: {
+            date: today,
+          },
+        });
+    
+        if (!day) {
+          day = await prisma.day.create({
+            data: {
+              date: today,
+            },
+          });
         }
-
+    
         const dayHabit = await prisma.dayHabit.findUnique({
+          where: {
+            day_id_habit_id: {
+              day_id: day.id,
+              habit_id: id,
+            },
+          },
+        });
+    
+        if (dayHabit) {
+          await prisma.dayHabit.delete({
             where: {
-                day_id_habit_id: {
-                    day_id: day.id,
-                    habit_id:id,
-                }
-            }
-        })
-
-        if(dayHabit){
-            //REMOVER A MARCAÇÃO DE 
-            await prisma.dayHabit.delete({
-                where:{
-                    id: dayHabit.id
-                }
-            })
-        }else{
-            await prisma.dayHabit.create({
-                data: {
-                    day_id: day.id,
-                    habit_id: id
-                }
-            })
+              id: dayHabit.id,
+            },
+          });
+        } else {
+          await prisma.dayHabit.create({
+            data: {
+              day_id: day.id,
+              habit_id: id,
+            },
+          });
         }
-
-        //completar o habito nesse dia
-        await prisma.dayHabit.create({
-            data:{
-                day_id: day.id,
-                habit_id: id,
-            }
-        })
-    })
+      });
+    
 
     app.get('/summary', async()=>{
         
